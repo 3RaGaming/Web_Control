9623174e5b57cffc5e36f96faf1e3c14b5aa1b3d
StudMuffin <wholikesgravy.questionmark@gmail.com>
2016-12-02 10:48:51 -0700

    Update web_control php
    Added <pre> to display lines better.
    Added steps to update process

--- html/update.sh
+++ html/update.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
+args=("$@");
 
-wget -q https://github.com/3RaGaming/Web_Control/archive/dev-bot-manage.zip \
-    -O dev-bot-manage.zip && unzip -u dev-bot-manage.zip && \
-    rsync -a -v Web_Control-dev-bot-manage/html/* ./ \
-    && rm -Rf dev-bot-manage.zip Web_Control-dev-bot-manage/
+if [ "${args[0]}" ]; then
+	case "${args[0]}" in
+	    'count')
+			echo "4"
+            ;;
+
+        '1')
+            wget -q https://github.com/3RaGaming/Web_Control/archive/dev-bot-manage.zip -O dev-bot-manage.zip
+            printf "4\r\n";
+            ;;
+
+        '2')
+            unzip -u dev-bot-manage.zip
+            printf "4\r\n";
+            ;;
+
+        '3')
+            rsync -a -v Web_Control-dev-bot-manage/html/* ./
+            printf "4\r\n";
+            ;;
+
+        '4')
+            rm -Rf dev-bot-manage.zip Web_Control-dev-bot-manage/
+            printf "4\r\n";
+            ;;
+
+        *)
+            printf "Error in input provided\r\n"
+            exit 1
+	esac
+else
+    printf "No input provided\r\n"
+fi
--- html/update_web_control.php
+++ html/update_web_control.php
@@ -11,20 +11,25 @@
 		}
 	}
 	header( 'Content-type: text/html; charset=utf-8' );
-	
+
 	if(isset($_SESSION['login']['level'])) { $user_level = $_SESSION['login']['level']; }  else { $user_level = "guest"; }
 	if(isset($_SESSION['login']['user'])) { $user_name = $_SESSION['login']['user']; }  else { $user_name = "guest"; }
-	
+
 	if($user_level=="admin") {
 		if(isset($_POST['update'])) {
 			if($_POST['update']=="yes") {
-				echo "Updating... <br />";
+				echo "<pre>Updating...\r\n";
 				ob_flush();
 				flush();
-				system('bash update.sh');
+				$count = system('bash update.sh count');
 				ob_flush();
 				flush();
-				echo "Done <br />";
+				for($n=1; $n<=$count; $n++) {
+					system('bash update.sh '.$n.'');
+					ob_flush();
+					flush();
+				}
+				echo "Done\r\n</pre>";
 				ob_flush();
 				flush();
 			}
