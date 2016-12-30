db449b1f5963d1682320e57f5cb03be03bba63da
StudMuffin <taytrho@gmail.com>
2016-12-29 23:46:18 -0700

    update for logs.php
    darn typos and apostrophe’s

--- html/getserver.php
+++ html/getserver.php
@@ -46,11 +46,11 @@ foreach(glob("$base_dir*", GLOB_ONLYDIR) as $dir) {
 			$server_select = $dir;
 			$server_tab_list = $server_tab_list . '
 			$("#server_list ul").append(\'<li><a href="#tabs-'.$server_select.'">'.$server_select.'</a></li>\');
-			$("#server_list").append("<div id="'.$server_select.'">Dynamic tab for '.$server_select.'</div>");';
+			$("#server_list").append(\'<div id="'.$server_select.'">Dynamic tab for '.$server_select.'</div>\');';
 		} else {
 			$server_tab_list = $server_tab_list . '
 			$("#server_list ul").append(\'<li><a href="#tabs-'.$dir.'">'.$dir.'</a></li>\');
-			$("#server_list").append("<div id="'.$dir.'">Dynamic tab for '.$dir.'</div>");';
+			$("#server_list").append(\'<div id="'.$dir.'">Dynamic tab for '.$dir.'</div>\');';
 		}
 	}
 }
