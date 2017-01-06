bb8e6739961506fba9ce0a0dbce3a034c48ac795
stuudmuffin <taytrho@gmail.com>
2017-01-05 17:33:18 -0700

    Log work
    Attempt to get file download working

--- html/logs.php
+++ html/logs.php
@@ -51,11 +51,124 @@
 				die();
 			}
 		}
-		if(isset($_REQUEST['filename'])) {
-			$file_path = $base_dir . $server_select . "/" . $_REQUEST['filename'];
-			if(file_exists($file_path)) {
-				#something
+		if(isset($_REQUEST['download'])&&isset($_REQUEST['d'])) {
+			$server_dir = $base_dir . $_REQUEST['d'] . "/logs";
+			if($_REQUEST['d']=="Managepgm") {
+				$server_dir = $base_dir . "logs";
 			}
+			if(file_exists($server_dir)) {
+				$file_path = $server_dir . $_REQUEST['download'];
+				if(file_exists($file_path)) {
+					if($_SESSION['login']['level']=="guest") {
+						die('Guests may not download files\nVisit our archive for file downloads\nwww.3ragaming.com/archive/factorio');
+					}
+					// file download found on http://www.media-division.com/php-download-script-with-resume-option/
+					// get the file request, throw error if nothing supplied
+					
+					// hide notices
+					@ini_set('error_reporting', E_ALL & ~ E_NOTICE);
+					
+					//- turn off compression on the server
+					@apache_setenv('no-gzip', 1);
+					@ini_set('zlib.output_compression', 'Off');
+					
+					// sanitize the file request, keep just the name and extension
+					// also, replaces the file location with a preset one ('./myfiles/' in this example)
+					$path_parts = pathinfo($file_path);
+					$file_name  = $path_parts['basename'];
+					$file_ext   = $path_parts['extension'];
+					// allow a file to be streamed instead of sent as an attachment
+					$is_attachment = isset($_REQUEST['stream']) ? false : true;
+					// make sure the file exists
+					if (is_file($file_path))
+					{
+						$file_size  = filesize($file_path);
+						$file = @fopen($file_path,"rb");
+						if ($file)
+						{
+							// set the headers, prevent caching
+							header("Pragma: public");
+							header("Expires: -1");
+							header("Cache-Control: public, must-revalidate, post-check=0, pre-check=0");
+							header("Content-Disposition: attachment; filename=\"$file_name\"");
+					
+							// set appropriate headers for attachment or streamed file
+							if ($is_attachment)
+									header("Content-Disposition: attachment; filename=\"$file_name\"");
+							else
+									header('Content-Disposition: inline;');
+					
+							// set the mime type based on extension, add yours if needed.
+							$ctype_default = "application/octet-stream";
+							$content_types = array(
+									"exe" => "application/octet-stream",
+									"zip" => "application/zip",
+									"tar.gz" => "application/tar+gzip"
+									);
+							$ctype = isset($content_types[$file_ext]) ? $content_types[$file_ext] : $ctype_default;
+							header("Content-Type: " . $ctype);
+							//check if http_range is sent by browser (or download manager)
+							if(isset($_SERVER['HTTP_RANGE'])) {
+								list($size_unit, $range_orig) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+								if ($size_unit == 'bytes') {
+									//multiple ranges could be specified at the same time, but for simplicity only serve the first range
+									//http://tools.ietf.org/id/draft-ietf-http-range-retrieval-00.txt
+									list($range, $extra_ranges) = explode(',', $range_orig, 2);
+								} else {
+									$range = '';
+									header('HTTP/1.1 416 Requested Range Not Satisfiable');
+									exit;
+								}
+							} else {
+								$range = '';
+							}
+							//figure out download piece from range (if set)
+							list($seek_start, $seek_end) = explode('-', $range, 2);
+					
+							//set start and end based on range (if set), else set defaults
+							//also check for invalid ranges.
+							$seek_end   = (empty($seek_end)) ? ($file_size - 1) : min(abs(intval($seek_end)),($file_size - 1));
+							$seek_start = (empty($seek_start) || $seek_end < abs(intval($seek_start))) ? 0 : max(abs(intval($seek_start)),0);
+					
+							//Only send partial content header if downloading a piece of the file (IE workaround)
+							if ($seek_start > 0 || $seek_end < ($file_size - 1)) {
+								header('HTTP/1.1 206 Partial Content');
+								header('Content-Range: bytes '.$seek_start.'-'.$seek_end.'/'.$file_size);
+								header('Content-Length: '.($seek_end - $seek_start + 1));
+							} else
+							header("Content-Length: $file_size");
+							header('Accept-Ranges: bytes');
+							set_time_limit(0);
+							fseek($file, $seek_start);
+							while(!feof($file)) {
+								print(@fread($file, 1024*8));
+								ob_flush();
+								flush();
+								if (connection_status()!=0) {
+									@fclose($file);
+									exit;
+								}			
+							}
+							// file save was a success
+							@fclose($file);
+							exit;
+						} else {
+							// file couldn't be opened
+							header("HTTP/1.0 500 Internal Server Error");
+							exit;
+						}
+					} else {
+						// file does not exist
+						header("HTTP/1.0 404 Not Found");
+						die('dead');
+						exit;
+					}
+					/*   END OF FILE DOWNLOAD */
+					//no reason to continue
+					die();
+				}
+			}
+			die();
 		}
 	}
 ?>
@@ -85,6 +198,10 @@
 				$('#server_list-' + server).html(html);
 				return false;
 			});
+		}
+		function Download(url) {
+			if (user_level == "guest") { return; }
+			document.getElementById('file_iframe').src = url;
 		}
 	</script>
 	<script type="text/javascript" language="javascript" src="assets/log-ui.js"></script>
