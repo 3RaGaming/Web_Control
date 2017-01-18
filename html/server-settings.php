976a383b1562710a4d012de6726d47408125f6c1
stuudmuffin <taytrho@gmail.com>
2017-01-18 13:59:06 -0700

    Server Settings Groundwork
    more bugfix

--- html/server-settings.php
+++ html/server-settings.php
@@ -114,7 +114,8 @@
 	</script>
 	<script type="text/javascript" language="javascript" src="assets/base.js"></script>
 	<script src="https://use.fontawesome.com/674cd09dad.js"></script>
-	<style type="text/css">@import "assets/base.css";</style>
+	<script type="text/javascript" language="javascript" src="assets/log-ui.js"></script>
+	<style type="text/css">@import "assets/log-ui.css";</style>
 </head>
 <body>
 	<div style="width: 99%; height: 99%;">
