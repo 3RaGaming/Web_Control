b308d42e5365746600518906a98f6df0e0cff315
zackman0010 <computerboyzack@gmail.com>
2017-01-23 00:22:00 -0600

    Committing first work
    I don't have a testing environment to work with this, so I have to
    commit to see any changes

---
+++ html/altlogin.php
@@ -0,0 +1,73 @@
+<?php
+if(!isset($_SESSION)) { session_start(); }
+if(isset($_SERVER["HTTPS"]) == false)
+{
+    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+    exit();
+	die();
+}
+//If logged in, and requested to logout... log them out and show login screen
+if(isset($_SESSION['login'])) {
+	if(isset($_REQUEST['logout'])) {
+		unset($_SESSION['login']);
+		$report = "<br />You have been logged out</br >";
+	} else {
+		//if not requesting to logout... Take back home
+		header("Location: ./?d=server1");
+		exit();
+		die();
+	}
+}
+
+$userN="";
+$passW="";
+if(isset($_POST['uname'])) {
+	$userN = addslashes($_POST['uname']);
+}
+if(isset($_POST['passw'])) {
+	$passW = addslashes(md5(trim($_POST['passw'])));
+}
+if(!empty($userN) && !empty($passW)) {
+	$userlist = file ('/var/www/users.txt');
+	$success = false;
+	foreach ($userlist as $user) {
+		$user_details = explode('|', $user);
+		if ((strtolower($user_details[0]) == strtolower($userN)) && trim($user_details[1]) == $passW) {
+			$userN = $user_details[0];
+			$userL = $user_details[2];
+			$success = true;
+			break;
+		}
+	}
+	if ($success) {
+		$_SESSION['login']['user']=$userN;
+		$_SESSION['login']['level']=$userL;
+		//Send home if logged in
+		header("Location: ./?d=server1");
+		die();
+	} else {
+		$report =  "<br />You have entered the wrong username or password. Please try again.<br />";
+	}
+} elseif(isset($_POST['submit'])) {
+	$report = "<br />I don't like no input<br />";
+}
+
+?>
+<html>
+<head>
+<link rel="stylesheet" media="all" href="assets/login.css" />
+</head>
+<body>
+<div class="login-page">
+  <div class="form">
+    <form class="login-form" name="login" method="post">
+		<input type="image" src="./3rabutton.png" />
+    </form>
+	<?php if(isset($report)) { echo $report; } ?>
+  </div>
+</div>
+</body>
+</html>
+<?php
+//End login page
+?>
